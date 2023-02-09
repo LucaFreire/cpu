fadb6fd159ed19f6d5bedb875a069766cd7f63c0
LucaFreire <lucasrodifreire@gmail.com>
2023-02-09 16:41:15 -0300

    add: PaintFullScreen

--- CPU/code.asm
+++ CPU/code.asm
@@ -1,8 +1,14 @@
-    mov     $1, 255
-loop:
-    inc     $0
-    cmp     $0, $1
-    je      end
-    jump    loop
-end:
-    jump    end
+PaintFullScreen:
+    mov     $0, 128
+    mov     $1, 5
+    shl     $0, $1 ; $0 = 4096
+    mov     $3, 255
+    mov     $4, 8
+    shl     $3, $4 ; $3 = mt grande
+    mov     $5, 255
+    add     $3, $5 ; $3 = 1111 1111 1111 1111
+    mov     [$0], $3 ; 4096 = 11111 11111 1 111 1 11 1 11 1 1 1 1 1 1 1 
+    loop:    
+        inc $0;
+        mov [$0], $3
+        jmp loop
